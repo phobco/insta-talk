6d639f13cac14ff6e8d3b4fc46c4a8f22d15fc97
Philipp Obrivanov <milliardex@gmail.com>
2022-01-30 02:49:45 +0300

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'rooms#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :rooms, only: %i[index show create], param: :token
 end
