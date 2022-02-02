2c10dc299e07e7c7b8ef7520a09803842c554145
Philipp Obrivanov <milliardex@gmail.com>
2022-02-03 00:07:29 +0300

    Remove enter/leave notifications

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -5,14 +5,10 @@ class RoomChannel < ApplicationCable::Channel
     @room = Room.find(params[:roomId])
     
     stream_from "room_channel_#{@room.id}"
-
-    speak('message' => '* * * присоединился к комнате * * *')
   end
 
   def unsubscribed
     logger.info 'Unsubscribed from RoomChannel'
-  
-    speak('message' => '* * * кокинул комнату  * * *')
   end
 
   def speak(data)
