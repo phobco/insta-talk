f61b31d29f8518c94634b35ec3b5a47bb642a8c8
Philipp Obrivanov <milliardex@gmail.com>
2022-02-03 00:05:55 +0300

    Refactor

--- app/services/message_service.rb
+++ app/services/message_service.rb
@@ -13,12 +13,11 @@ class MessageService
   private
 
   def create_message!
-    @message ||= Message.create! body: @body, room: @room, user: @user
+    @message ||= Message.create!(body: @body, room: @room, user: @user)
   end
 
   def broadcast_message
-    ActionCable.server.broadcast "room_channel_#{@room.id}",
-      { message: render_message }
+    ActionCable.server.broadcast("room_channel_#{@room.id}", { message: render_message })
   end
 
   def render_message
