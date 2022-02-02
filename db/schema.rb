4b7c7640861424c895116f48aabd225b26920e1c
Philipp Obrivanov <milliardex@gmail.com>
2022-02-03 02:57:10 +0300

    Add online column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_234829) do
+ActiveRecord::Schema.define(version: 2022_02_02_235032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2022_02_02_234829) do
     t.string "nickname"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "online", default: false, null: false
   end
 
   add_foreign_key "messages", "rooms"
