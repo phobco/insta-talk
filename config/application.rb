16255337ce19a0b3c09590087b8311333b2eb4bd
Philipp Obrivanov <milliardex@gmail.com>
2022-02-06 20:12:01 +0300

    Set time zone

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,18 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
+require 'active_model/railtie'
 # require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-# require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
-# require "rails/test_unit/railtie"
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+# require "action_mailer/railtie'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
+require 'action_cable/engine'
+# require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,6 +24,7 @@ module Instatalk
     config.load_defaults 7.0
 
     config.i18n.default_locale = :ru
+    config.time_zone = 'Moscow'
 
     # Configuration for the application, engines, and railties goes here.
     #
